bb358e3831e37403b580f3a62389058c34bc362f
Robert Burdsall <burdsall25r@ncssm.edu>
2024-03-03 08:47:31 -0500

    robert's commit

--- djangoserver/mysite/mysite/settings.py
+++ djangoserver/mysite/mysite/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for mysite project.
 
@@ -63,10 +64,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "mysite.urls"
 
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- djangoserver/mysite/mysite/urls.py
+++ djangoserver/mysite/mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
+from main.views import index
 
 urlpatterns = [
+    path('', index, name='index'),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+
 ]
