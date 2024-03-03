2a8f36fa01f5fd839c711d818e640d0230bf083a
Robert Burdsall <burdsall25r@ncssm.edu>
2024-03-02 19:43:59 -0500

    update

--- main.py
+++
@@ -1,9 +0,0 @@
-from altair import LayerSpec
-import geopandas as gpd
-import fiona
-
-layerslist = []
-for layer in fiona.listlayers('Resources/NC_Northern_slr_final_dist.gdb/'):
-    layerslist.append(layer)
-
-print(gpd.read_file("Resources/NC_Northern_slr_final_dist.gdb/", driver='FileGDB', layer=0))
